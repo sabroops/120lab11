2ef9ddd5e0dde5e8265aa3ce315218e39c7e26fb
Sabrina Carlos <scarl011@ucr.edu>
2020-06-07 19:47:14 -0700

    weeoeoeoeooe

--- turnin/scarl011_lab11_part2.c
+++ turnin/scarl011_lab11_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Sabrina Carlos
- *  Partner(s) Name: 
+ *      Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #11  Exercise #1
+ *	Assignment: Lab #11  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -16,21 +16,21 @@
 #include "bit.h"
 #include "keypad.h"
 #include "scheduler.h"
-#include "io.c"
 #include "io.h"
 
-
 unsigned char x = 0x00;
+unsigned char leg[] = "CS120B is Legend... wait for it DARY!               "; //barney boi
+unsigned char out[16] = ""; //fit on screen
+unsigned char count = 0x00;
 
-enum keypad_states{Start, key};
-
-int keypadSMTick(int state){
-	switch(state){
+enum Screen_states { Start, disp };
+int ScreenSMTick(int state) {
+	switch(state) {
 		case Start:
-			state = key;
+			state = disp;
 			break;
-		case key:
-		       	state = key;
+		case disp:
+		       	state = disp;
 			break;
 		default: state = Start;
 			 break;
@@ -38,53 +38,38 @@ int keypadSMTick(int state){
 	switch (state) {
 		case Start: 
 			break;
-		case key:
-			x = GetKeypadKey();
+		case disp:
+			for (unsigned int i = 0; i < 16; i++) {
+				out[i] = leg[(count + i) % 52];	
+			}
+			LCD_DisplayString(1, out);
+			count++;
+			if (count > 52) { 
+				count = 0;
+			}
 			break;
 		default:
 			break;
 	}
-	switch (x){
-		case '\0': PORTB = 0x1F; break;
-		case '1': PORTB = 0x01; break;
-		case '2': PORTB = 0x02; break;
-		case '3': PORTB = 0x03; break;
-		case '4': PORTB = 0x04; break;
-		case '5': PORTB = 0x05; break;
-		case '6': PORTB = 0x06; break;
-		case '7': PORTB = 0x07; break;
-		case '8': PORTB = 0x08; break;
-		case '9': PORTB = 0x09; break;
-		case 'A': PORTB = 0x0A; break;
-		case 'B': PORTB = 0x0B; break;
-		case 'C': PORTB = 0x0C; break;
-		case 'D': PORTB = 0x0D; break;
-		case '*': PORTB = 0x0E; break;
-		case '0': PORTB = 0x00; break;
-		case '#': PORTB = 0x0F; break;
-		default: PORTB = 0X1B; break;
-	}
 	return state;
 }
-
 int main(void) {
     /* Insert DDR and PORT initializations */
-        DDRB = 0xFF; PORTB = 0x00;
-        DDRC = 0xF0; PORTC = 0x0F;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
 	static task task1;
 	task *tasks[] = {&task1};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-	const char start = -1;
-
+	//const char start = -1;
 	task1.state = Start;
-	task1.period = 10;
+	task1.period = 1500;
 	task1.elapsedTime = task1.period;
-	task1.TickFct = &keypadSMTick;
+	task1.TickFct = &ScreenSMTick;
 
 	TimerSet(1);
 	TimerOn();
-	
+	LCD_init();
 	unsigned short i;
     while (1) {
 	for (i=0; i<numTasks; i++){
---
+++ turnin/scarl011_lab11_part4.c
@@ -0,0 +1,224 @@
+/*	Author: Sabrina Carlos
+ *      Partner(s) Name: 
+ *	Lab Section: 021
+ *	Assignment: Lab #11  Exercise #4
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include<avr/interrupt.h>
+#include "timer.h"
+#include "bit.h"
+#include "keypad.h"
+#include "scheduler.h"
+#include "io.h"
+
+unsigned char cursy = 0x00;
+enum keypad_states{start, key};
+
+int keypadSMTick(int state){
+	unsigned char x = 0x00; //i guess...
+	x = GetKeypadKey();
+	switch(state){
+		case start:
+			state = key;
+			break;
+		case key:
+		       	state = key;
+			break;
+		default: state = start;
+			 break;
+	}
+	switch (state) {
+		case start: 
+			break;
+		case key:
+			switch(x) { // really just have to ctrl v functions each time so it stays
+				case '\0':
+					x = 0x00;
+					break;
+				case '0':
+					x = 0x00;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+					break;	
+				case '1':
+					x = 0x01;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+					break;
+				case '2':
+					x = 0x02;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+					break;
+				case '3':
+                                        x = 0x03;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+                                        break;	
+				case '4':
+                                        x = 0x04;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+                                        break;
+                                case '5':
+					x = 0x05;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+					break;
+                                case '6':         
+                                        x = 0x06;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+                                        break;
+                                case '7':
+                                        x = 0x07;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+                                        break;
+                                case '8':
+                                        x = 0x08;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+                                        break;
+				case '9':
+					x = 0x09;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x+'0');
+					break;
+                                case 'A':
+                                        x = 65;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;
+                                case 'B':
+                                        x = 66;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;
+                                case 'C':
+                                        x = 67;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;
+                                case 'D':
+                                        x = 68;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;
+                                case '#':
+                                        x = 35;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;
+                                case '*':         
+                                        x = 42;
+					if(cursy>22) { cursy = 0;}
+					else(cursy++);
+					LCD_Cursor(cursy);
+					LCD_WriteData(x);
+                                        break;	
+			}
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+/*
+enum Screen_states { Start, disp };
+int ScreenSMTick(int state) {
+	switch(state) {
+		case Start:
+			state = disp;
+			break;
+		case disp:
+		       	state = disp;
+			break;
+		default: state = Start;
+			 break;
+	}
+	switch (state) {
+		case Start: 
+			break;
+		case disp:
+			LCD_Cursor(1);
+			LCD_WriteData(x + '0');
+			break;
+		default:
+			break;
+	}
+	return state;
+}*/
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; //HOW DID I FORGET THIS.....
+    /* Insert your solution below */
+	static task task1;
+	task *tasks[] = {&task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+	const char start = -1;
+
+	task1.state = start;
+	task1.period = 10;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &keypadSMTick;
+
+	TimerSet(200);
+	TimerOn();
+	LCD_init();
+    LCD_DisplayString(1, "Never Gonna Give You Up");
+
+	unsigned short i;
+
+    	while (1) {
+	for (i=0; i<numTasks; i++){
+		if (tasks[i]->elapsedTime == tasks[i]->period){
+			tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			tasks[i]->elapsedTime = 0;
+		}
+		tasks[i]->elapsedTime += 10;
+	}
+	while (!TimerFlag);
+	TimerFlag = 0;
+    }
+    //return 1;
+}
